509d24618b708cada7e88927b9acfd549e52218a
Iliass Raihani <i.raihani@outlook.com>
2021-01-23 19:34:43 +0100

    chromely without vuejs

---
+++ Link.Chromely/MyChromelyApp.cs
@@ -0,0 +1,14 @@
+﻿using Chromely;
+using Chromely.Core;
+
+namespace ChromelyActions
+{
+    class MyChromelyApp : ChromelyBasicApp
+    {
+        public override void Configure(IChromelyContainer container)
+        {
+            base.Configure(container);
+            // other container configuration can be placed here
+        }
+    }
+}
---
+++ Link.Chromely/Program.cs
@@ -0,0 +1,17 @@
+﻿using Chromely.Core;
+
+namespace ChromelyActions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // basic example of the application builder
+            AppBuilder
+            .Create()
+            .UseApp<MyChromelyApp>()
+            .Build()
+            .Run(args);
+        }
+    }
+}
