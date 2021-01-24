b8639ae68322d6c0b3b801ed03bde1536c5baa2c
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2021-01-24 14:26:13 +0100

    evenement table created

---
+++ Link.Database/evenement.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[evenement]
+(
+	[Id] INT NOT NULL , 
+    [user_id] INT NOT NULL, 
+    [subject] NCHAR(10) NOT NULL, 
+    [start_at] TIME(0) NOT NULL, 
+    [end_at] TIME(0) NOT NULL, 
+    PRIMARY KEY ([Id])
+)
