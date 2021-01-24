9aaae366e57dd442607a257d43754ec5feb3f403
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2021-01-24 14:24:31 +0100

    user table created

---
+++ Link.Database/user.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[user]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [username] VARCHAR(50) NOT NULL, 
+    [password] TEXT NOT NULL, 
+    [email] VARCHAR(255) NOT NULL, 
+    [access] INT NOT NULL
+)
