923a0add920dd5a22952c8f21d4f4adef5c9373d
osaelh <oussamaelh@gmail.com>
2021-01-28 16:20:34 +0100

    DataAccess class

---
+++ ModelsLibrary/Utilities/DataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Dapper;
+
+namespace ModelsLibrary.Utilities
+{
+    public class DataAccess : IDataAccess
+    {
+        public List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cn = new System.Data.SqlClient.SqlConnection(LoadConnectionString()))
+            {
+                var output = cn.Query<T>(sql, new DynamicParameters()).ToList();
+                return output;
+            }
+        }
+
+        public void SaveData<T>(T user, string sql)
+        {
+            using (IDbConnection cn = new System.Data.SqlClient.SqlConnection(LoadConnectionString()))
+            {
+                cn.Execute(sql, user);
+            }
+        }
+
+        public void UpdateData<T>(T user, string sql)
+        {
+            using (IDbConnection cn = new System.Data.SqlClient.SqlConnection(LoadConnectionString()))
+            {
+                cn.Execute(sql, user);
+            }
+        }
+        public string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
---
+++ ModelsLibrary/Utilities/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ModelsLibrary.Utilities
+{
+    public interface IDataAccess
+    {
+        string LoadConnectionString(string id = "Default");
+        List<T> LoadData<T>(string sql);
+        void SaveData<T>(T user, string sql);
+        void UpdateData<T>(T user, string sql);
+    }
+}
